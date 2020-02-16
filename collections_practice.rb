2fe55eb07ab49bbbd34be6aff07bf20423b06c3f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-16 00:26:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,14 +7,14 @@ def sort_array_desc(array)
  array.sort.reverse
 end
 
-# def sort_array_char_count(array)
+def sort_array_char_count(array)
   
-# array.sort do |a, b|
-#   if a == b
-#     0
-#   elsif a < b
-#     -1
-#   elsif a > b
-#     1
-#   end
-# end
+array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+end
